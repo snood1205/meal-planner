3e357bc74e0d4053c16e575ca6e1cc1993820f85
Eli <9064062+snood1205@users.noreply.github.com>
2023-11-30 19:02:40 -0500

    Change API_URL access

--- frontend/src/utilities/fetch-endpoint.ts
+++ frontend/src/utilities/fetch-endpoint.ts
@@ -1,4 +1,4 @@
-const apiUrl = import.meta.env.API_URL;
+const apiUrl = process.env.API_KEY ?? "http://localhost:3000";
 
 export const fetchEndpoint = async (endpoint: string, init?: RequestInit): Promise<Response> => {
   const url = apiUrl + endpoint;
